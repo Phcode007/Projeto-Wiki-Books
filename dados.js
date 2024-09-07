3cbebb892b1e4d63f30b2df3cfe52498fac4744a
Phcode007 <149791777+Phcode007@users.noreply.github.com>
2024-09-07 19:11:58 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    titulo: "1984",
+    descricao:
+      "1984 é um romance distópico de George Orwell que retrata um futuro sombrio e totalitário. A história se passa em uma Londres futurista, agora parte da Oceânia, um Estado totalitário onde a vigilância é constante e a liberdade individual é completamente suprimida.",
+    link: "https://pt.wikipedia.org/wiki/1984_(livro)",
+    tags: "futuro distópico romance suspense",
+  },
+  {
+    titulo: "Do mil ao milhão",
+    descricao:
+      "Do Mil ao Milhão é um guia prático de educação financeira escrito por Thiago Nigro, mais conhecido como Primo Rico. O livro tem como objetivo principal ensinar as pessoas a alcançarem a liberdade financeira, mesmo começando com pouco dinheiro.",
+    link: "https://www.mobills.com.br/blog/educacao-financeira/resumo-livro-do-mil-ao-milhao/#:~:text=Na%20verdade%2C%20a%20intenção%20dele,mesmo%20que%20ganhemos%20pouco%20atualmente.",
+    tags: "educação financeira",
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao:
+      "O Poder da Autorresponsabilidade é um livro escrito por Paulo Vieira que tem como objetivo central promover uma transformação pessoal profunda. A obra incentiva os leitores a assumirem o controle de suas vidas, abandonando a postura de vítimas e passando a acreditar no próprio potencial.",
+    link: "https://www.donosdamidia.com.br/resumo-do-livro-o-poder-da-autorresponsabilidade/#:~:text=%E2%80%9CO%20Poder%20da%20Autorresponsabilidade%E2%80%9D%20de%20Paulo%20Vieira%20%C3%A9%20um%20guia,v%C3%ADtima%20e%20assumirem%20o%20controle",
+    tags: " transformação performance",
+  },
+];
