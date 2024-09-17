5df4953d2293e53c4beb9b6b223edd41b4bbea00
Phcode007 <149791777+Phcode007@users.noreply.github.com>
2024-09-17 19:40:49 -0300

    Novas obras adcionadas ao bando de conhecimento da aplicação

--- dados.js
+++ dados.js
@@ -3,13 +3,15 @@ let dados = [
     titulo: "1984",
     descricao:
       "1984 é um romance distópico de George Orwell que retrata um futuro sombrio e totalitário. A história se passa em uma Londres futurista, agora parte da Oceânia, um Estado totalitário onde a vigilância é constante e a liberdade individual é completamente suprimida.",
+    autor: "George Orwell",
     link: "https://pt.wikipedia.org/wiki/1984_(livro)",
-    tags: "futuro distópico romance suspense",
+    tags: "futuro distópico romance suspense George Orwell",
     linkof:
       "https://www.amazon.com.br/1984-George-Orwell/dp/6589678006/ref=sr_1_5?__mk_pt_BR=ÅMÅŽÕÑ&crid=136CY5FRGFZ81&dib=eyJ2IjoiMSJ9.YmAQI_v2aNNn_GqixoPw_VtPI44gKZMVXs193hP-7L3N_jltRjHPWBy1r2kzdSO32Ef4jIdUaJowcQo9hHkKuhIfx1esa294dcLBs6nnDJH0hRcdS_t78bl2sorQOG2upd2fJQbJgoNFfrxFgxCOkOnb3amT8AcxJXuT2Z_NvbZS1TXAlnhpSGCLa0C96ES906Cp_O6k5-Slirkky9ZIbxd3EotM1Oc5gsxgxYPGFmCOhul06YcscNpdwx41G5yEGB6FLwkmxxYfz-9SenOhZydHO9_s2sy-Y39Z_wt838I.3XCYZ7kqUIJ3RhV1zMaSGWPp7oU9pg8qo-rbmnhkOd4&dib_tag=se&keywords=1984&qid=1726595233&sprefix=1984%2Caps%2C302&sr=8-5",
   },
   {
     titulo: "Do mil ao milhão",
+    autor: "Thiago Nigro",
     descricao:
       "Do Mil ao Milhão é um guia prático de educação financeira escrito por Thiago Nigro, mais conhecido como Primo Rico. O livro tem como objetivo principal ensinar as pessoas a alcançarem a liberdade financeira, mesmo começando com pouco dinheiro.",
     link: "https://www.mobills.com.br/blog/educacao-financeira/resumo-livro-do-mil-ao-milhao/#:~:text=Na%20verdade%2C%20a%20intenção%20dele,mesmo%20que%20ganhemos%20pouco%20atualmente.",
@@ -19,6 +21,7 @@ let dados = [
   },
   {
     titulo: "O poder da Autorresponsabilidade",
+    autor: "Paulo Vieira",
     descricao:
       "O Poder da Autorresponsabilidade é um livro escrito por Paulo Vieira que tem como objetivo central promover uma transformação pessoal profunda. A obra incentiva os leitores a assumirem o controle de suas vidas, abandonando a postura de vítimas e passando a acreditar no próprio potencial.",
     link: "https://www.donosdamidia.com.br/resumo-do-livro-o-poder-da-autorresponsabilidade/#:~:text=%E2%80%9CO%20Poder%20da%20Autorresponsabilidade%E2%80%9D%20de%20Paulo%20Vieira%20%C3%A9%20um%20guia,v%C3%ADtima%20e%20assumirem%20o%20controle",
@@ -28,6 +31,7 @@ let dados = [
   },
   {
     titulo: "Memórias de um sargento de milícias",
+    autor: "Manuel Antônio de Almeida",
     descricao:
       "A história gira em torno de Leonardo, um menino malandro e esperto que, ao longo da narrativa, se transforma em um sargento das milícias. O romance acompanha suas aventuras e desventuras, desde a infância até a vida adulta, retratando o cotidiano da cidade, os costumes da época e os tipos sociais que a povoavam.",
     link: "https://brasilescola.uol.com.br/literatura/memorias-de-um-sargento-de-milicias.htm#:~:text=Publicado%2C%20pela%20primeira%20vez%2C%20em,%2C%20assim%2C%20não%20é%20perfeito.",
@@ -37,6 +41,7 @@ let dados = [
   },
   {
     titulo: "O poder da Ação",
+    autor: "Paulo Vieira",
     descricao:
       "O Poder da Ação, de Paulo Vieira, é um guia prático que busca despertar no leitor a importância de transformar ideias em ações concretas. O autor, um renomado coach, propõe um método eficaz para superar a procrastinação e alcançar resultados significativos em todas as áreas da vida.",
     link: "https://montecarloalimentos.com.br/blog/o-poder-da-acao/#:~:text=Metas%20claras%20e%20objetivas%3A,ação%20concreto%20para%20alcançá-las.",
@@ -46,6 +51,7 @@ let dados = [
   },
   {
     titulo: "Orgulho e Preconceito",
+    autor: "Jane Austen",
     descricao:
       "Orgulho e Preconceito, de Jane Austen, é um clássico da literatura inglesa que narra a história de amor entre Elizabeth Bennet, uma jovem inteligente e espirituosa, e Fitzwilliam Darcy, um homem rico e orgulhoso.",
     link: "https://admiravelleitura.wordpress.com/2020/07/17/orgulho-e-preconceito-jane-austen/#:~:text=Sua%20primeira%20publicação%20data%20do,é%20a%20audácia%2C%20inteligência%20e",
@@ -60,6 +66,75 @@ let dados = [
     link: "https://www.dominiopublico.gov.br/download/texto/bv000009.pdf",
     tags: "romance literatura brasileira literatura clássica Joaquim Manuel de Macedo",
     linkof:
-      "https://www.amazon.com.br/Moreninha-Joaquim-Manuel-Macedo/dp/8535918561/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575854867", // Link para a compra do livro na Amazon
+      "https://www.amazon.com.br/Moreninha-Joaquim-Manuel-Macedo/dp/8535918561/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575854867",
+  },
+  {
+    titulo: "A Revolução dos Bichos",
+    descricao:
+      "Fábula política escrita por George Orwell, A Revolução dos Bichos narra a história de uma revolução em uma granja, onde os animais se rebelam contra seus opressores humanos. A narrativa serve como uma alegoria crítica aos regimes totalitários do século XX.",
+    link: "https://www.todamateria.com.br/a-revolucao-dos-bichos/",
+    tags: "fábula política literatura clássica George Orwell distopia ",
+    linkof:
+      "https://www.amazon.com.br/Revolução-dos-Bichos-George-Orwell/dp/8535918561/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575854867",
+  },
+  {
+    titulo: "O Alienista",
+    autor: "Machado de Assis",
+    descricao:
+      "Em 'O Alienista', Machado de Assis satiriza a sociedade brasileira através da história de um médico que decide isolar todos os considerados loucos da cidade. A obra explora temas como a loucura, a razão e o poder, com a ironia e o humor característicos do autor.",
+    link: "https://www.dominiopublico.gov.br/download/texto/bv000009.pdf",
+    tags: "realismo literatura brasileira Machado de Assis sátira loucura razão poder",
+    linkof:
+      "https://www.amazon.com.br/Alienista-Machado-Assis/dp/8535918561/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575854867",
+  },
+  {
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    descricao:
+      "Em 'Dom Casmurro', Machado de Assis narra a história de Bento Santiago, um homem atormentado pela dúvida sobre a fidelidade de sua esposa, Capitu. Através de um narrador em primeira pessoa, a obra explora temas como ciúme, traição, memória e a construção da identidade.",
+    link: "https://guiadoestudante.abril.com.br/estudo/dom-casmurro-resumo-obra-de-machado-de-assis",
+    tags: "realismo literatura brasileira Machado de Assis ciúme traição ",
+    linkof:
+      "https://www.amazon.com.br/Dom-Casmurro-Machado-Assis/dp/8535918561/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575854867",
+  },
+  {
+    titulo: "Memórias Póstumas de Brás Cubas",
+    autor: "Machado de Assis",
+    descricao:
+      "Em 'Memórias Póstumas de Brás Cubas', Machado de Assis revoluciona a narrativa ao contar a história de sua vida já após a morte. Brás Cubas, um homem acomodado e crítico da sociedade, reflete sobre suas escolhas e fracassos, inaugurando o Realismo no Brasil com sua ironia e pessimismo.",
+    link: "https://www.dominiopublico.gov.br/download/texto/bv000009.pdf",
+    tags: "realismo literatura brasileira Machado de Assis ironia pessimismo narrador não confiável",
+    linkof:
+      "https://www.amazon.com.br/Memorias-Postumas-De-Bras-Cubas-Via/dp/8567097183/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575854867",
+  },
+  {
+    titulo: "Vidas Secas",
+    autor: "Graciliano Ramos",
+    descricao:
+      "Em 'Vidas Secas', Graciliano Ramos retrata a dura realidade de uma família de retirantes nordestinos em busca de sobrevivência em um sertão árido. A obra explora temas como a seca, a miséria, a esperança e a luta pela sobrevivência, com uma linguagem simples e direta.",
+    link: "https://www.todamateria.com.br/resumo-e-analise-de-memorias-postumas-de-bras-cubas/",
+    tags: "regionalismo literatura brasileira Graciliano Ramos seca miséria sertão",
+    linkof:
+      "https://www.amazon.com.br/Vidas-Secas-Graciliano-Ramos/dp/8535918561/ref/sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575858467",
+  },
+  {
+    titulo: "Alguma Poesia",
+    autor: "Carlos Drummond de Andrade",
+    descricao:
+      "Publicado em 1930, 'Alguma Poesia' marca a estreia de Carlos Drummond de Andrade e se tornou um clássico da literatura brasileira. A obra apresenta um retrato íntimo e reflexivo do poeta, abordando temas como a vida cotidiana, a passagem do tempo, a solidão e a busca por identidade. Com uma linguagem simples e direta, Drummond explora a condição humana de forma universal.",
+    link: "https://guiadoestudante.abril.com.br/estudo/alguma-poesia-resumo-e-analise-da-obra-de-carlos-drummond-de-andrade",
+    tags: "modernismo literatura brasileira Carlos Drummond de Andrade poesia brasileira",
+    linkof:
+      "https://www.amazon.com.br/Alguma-poesia-Carlos-Drummond-Andrade/dp/6555874619/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575858467", // **Verifique e ajuste o link da Amazon**
+  },
+  {
+    titulo: "Iracema",
+    autor: "José de Alencar",
+    descricao:
+      "Iracema, a virgem dos lábios de mel, é um romance indianista que narra a história de amor entre a índia tabajara e o português Martim Soares Moreno. A obra, marcada pela linguagem poética e pela idealização do índio, retrata o encontro entre duas culturas e a formação do povo brasileiro. Através de um enredo envolvente e descrições exuberantes da natureza, Alencar constrói um mito de fundação da nação brasileira.",
+    link: "https://www.todamateria.com.br/iracema/",
+    tags: "romantismo romance literatura brasileira José de Alencar indianismo mito de fundação",
+    linkof:
+      "https://www.amazon.com.br/Iracema-Jos%C3%A9-Alencar/dp/8578885252/ref=sr_1_1?crid=213023025&keywords=a+moreninha&qid=1575858467",
   },
 ];
